4d0c6b14f251f5568d9ed78948e1dd5043b59255
Enrique Garcia <engapa@gmail.com>
2017-12-11 12:44:19 +0100

    Get thrift model by curl from original project

--- start_frontend.sh
+++ start_frontend.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
-mkdir -p './thrift'
+mkdir -p thrift
 
-docker run -v $(pwd)/..:/data thrift:0.10 \
-        thrift -r -v -out /data/frontend/thrift --gen js:node /data/thrift/ModelDB.thrift
+curl -Lo ModelDB.thrift https://raw.githubusercontent.com/mitdbg/modeldb/master/thrift/ModelDB.thrift
+
+docker run -v $(pwd):/data thrift:0.10 \
+        thrift -r -v -out /data/thrift --gen js:node /data/ModelDB.thrift
 
 npm install
 npm start
