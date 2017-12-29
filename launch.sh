6dcf04cc6466d0d50a2531e495ebb0c2b1c5c643
Enrique Garcia <engapa@gmail.com>
2017-12-29 12:52:34 +0100

    All operations are available al launch.sh script

--- launch.sh
+++ launch.sh
@@ -3,33 +3,34 @@
 set -e
 
 MDB_THRIFT_LOCAL=${MDB_THRIFT_LOCAL:-"false"}
-MDB_THRIFT_GEN=${MDB_THRIFT_GEN:-"true"}
 MDB_THRIFT_VERSION=${MDB_THRIFT_VERSION:-"0.10.0"}
 MDB_THRIFT_MODEL_VERSION=${MDB_THRIFT_MODEL_VERSION:-"master"}
 MDB_WAIT_BACKEND=${MDB_WAIT_BACKEND:-"true"}
 MDB_BACKEND_HOST=${MDB_BACKEND_HOST:-"localhost"}
 
+MDB_DOCKER_NAME=${MDB_DOCKER_NAME:-"modeldb-frontend"}
+MDB_DOCKER_NAME_TAG=${MDB_DOCKER_NAME_TAG:-"latest"}
+MDB_DOCKER_OPTS=${MDB_DOCKER_OPTS:-"--net=host"}
+
 function gen() {
 
   curl -Lo ModelDB.thrift https://raw.githubusercontent.com/mitdbg/modeldb/${MDB_THRIFT_MODEL_VERSION}/thrift/ModelDB.thrift
 
-  if [[ "${MDB_THRIFT_GEN}" == "true" ]]; then
-    mkdir -p thrift
-    if [[ "$MDB_THRIFT_LOCAL" == 'true' ]]; then
-      if [[ ! "$(thrift -version)" ]]; then
-        pushd ~/
-        wget -q http://archive.apache.org/dist/thrift/${MDB_THRIFT_VERSION}/thrift-${MDB_THRIFT_VERSION}.tar.gz
-        tar -xzf thrift-${MDB_THRIFT_VERSION}.tar.gz
-        cd thrift-${MDB_THRIFT_VERSION} && ./configure --without-python && make
-        ln -n ~/thrift-${MDB_THRIFT_VERSION}/compiler/cpp/thrift /usr/local/bin/thrift
-        popd
-        thrift -version
-      fi
-      thrift -r -out 'thrift' -gen js:node 'ModelDB.thrift'
-    else
-      docker run -v $(pwd):/data thrift:${MDB_THRIFT_VERSION} \
-        thrift -r -v -out /data/thrift --gen js:node /data/ModelDB.thrift
+  mkdir -p thrift
+  if [[ "$MDB_THRIFT_LOCAL" == 'true' ]]; then
+    if [[ ! "$(thrift -version)" ]]; then
+      pushd ~/
+      wget -q http://archive.apache.org/dist/thrift/${MDB_THRIFT_VERSION}/thrift-${MDB_THRIFT_VERSION}.tar.gz
+      tar -xzf thrift-${MDB_THRIFT_VERSION}.tar.gz
+      cd thrift-${MDB_THRIFT_VERSION} && ./configure --without-python && make
+      ln -n ~/thrift-${MDB_THRIFT_VERSION}/compiler/cpp/thrift /usr/local/bin/thrift
+      popd
+      thrift -version
     fi
+    thrift -r -out 'thrift' -gen js:node 'ModelDB.thrift'
+  else
+    docker run --rm -v $(pwd):/data thrift:${MDB_THRIFT_VERSION} \
+      thrift -r -v -out /data/thrift --gen js:node /data/ModelDB.thrift
   fi
 
 }
@@ -63,21 +64,75 @@ function stop() {
 
 }
 
+function docker-build() {
+
+  docker build -t modeldb-frontend:latest \
+    --label modeldb=frontend \
+    --build-arg MDB_THRIFT_LOCAL=$MDB_THRIFT_LOCAL \
+    --build-arg MDB_THRIFT_VERSION=$MDB_THRIFT_VERSION \
+    --build-arg MDB_THRIFT_MODEL_VERSION=$MDB_THRIFT_MODEL_VERSION .
+
+}
+
+function docker-start() {
+
+    [[ $(docker ps -a -f "name=$MDB_DOCKER_NAME" --format '{{.Names}}') == $MDB_DOCKER_NAME ]] && \
+       (echo "Container $MDB_DOCKER_NAME already exists"; exit 1)
+
+    MDB_DOCKER_OPTS="${MDB_DOCKER_OPTS} -e MDB_WAIT_BACKEND=${MDB_WAIT_BACKEND} -e MDB_BACKEND_HOST=${MDB_BACKEND_HOST} "
+
+    if [[ "${MDB_THRIFT_LOCAL}" == 'false' ]]; then
+       MDB_DOCKER_OPTS="${MDB_DOCKER_OPTS} -v $(pwd)/thrift "
+    fi
+
+    docker run -it $MDB_DOCKER_OPTS --name $MDB_DOCKER_NAME $MDB_DOCKER_NAME:$MDB_DOCKER_NAME_TAG
+
+}
+
+function docker-stop() {
+
+    [[ $(docker ps -f "name=$MDB_DOCKER_NAME" --format '{{.Names}}') == $MDB_DOCKER_NAME ]] && \
+       docker stop $MDB_DOCKER_NAME
+
+}
+
+function clean-all() {
+
+    [ -d thrift ] && rm -r ./thrift
+
+    [[ $(docker ps -a -f "name=$MDB_DOCKER_NAME" --format '{{.Names}}') == $MDB_DOCKER_NAME ]] && \
+       docker rm -f $MDB_DOCKER_NAME
+
+    IMAGES_TO_DEL=$(docker images -f label="modeldb=frontend" --format '{{.ID}}')
+    if [[ $IMAGES_TO_DEL ]]; then docker rmi $IMAGES_TO_DEL; fi
+
+}
+
 # Main options
 case "$1" in
   gen)
-        shift
         gen
         ;;
   start)
-        shift
         start
         ;;
   stop)
         stop
         exit $?
         ;;
+  docker-build)
+        docker-build
+        ;;
+  docker-start)
+        docker-start
+        ;;
+  docker-stop)
+        docker-start
+        ;;
+  clean-all)
+        clean-all
+        ;;
   *)
-        echo "Usage: $0 {gen|start|stop}"
+        echo "Usage: $0 {gen|start|docker-build|docker-start|stop|docker-stop|clean-all}"
         exit 1
 esac
